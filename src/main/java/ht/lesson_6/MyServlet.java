6be1a89583ae7db9410d83a5278b780425cd47f6
Aleksandr Shakhoff <sas7289@yandex.ru>
2021-09-26 23:05:04 +0300

    all ready

---
+++ src/main/java/ht/lesson_6/MyServlet.java
@@ -0,0 +1,18 @@
+package ht.lesson_6;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "gb", urlPatterns = "/homeTask_5")
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().println("Hello world");
+    }
+}
