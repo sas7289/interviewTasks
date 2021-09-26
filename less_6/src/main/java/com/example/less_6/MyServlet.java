8f6807cd41e9d0fb4a023321a647ac9610122d6c
Aleksandr Shakhoff <sas7289@yandex.ru>
2021-09-26 23:22:59 +0300

    all ready

--- less_6/src/main/java/com/example/less_6/MyServlet.java
+++ less_6/src/main/java/com/example/less_6/MyServlet.java
@@ -1,10 +1,12 @@
 package com.example.less_6;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 @WebServlet(name = "gb", urlPatterns = "/hello")
@@ -12,6 +14,6 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().println("Hello world");
+        resp.getWriter().println("<h1>Hello world</h1>");
     }
 }
