9c0e576c06ba7bfadea35bd0e39ee1d77082fa6e
Aleksandr Shakhoff <sas7289@yandex.ru>
2021-09-26 23:05:28 +0300

    all ready

--- src/main/java/ht/lesson_6/MyServlet.java
+++ src/main/java/ht/lesson_6/MyServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(name = "gb", urlPatterns = "/homeTask_5")
+@WebServlet(name = "gb", urlPatterns = "/hello")
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
